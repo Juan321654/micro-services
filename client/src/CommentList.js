cad7a82c9221ca631d640571bb333e8b4d3be694
juan camacho <juanc@tiv.solutions>
2022-01-30 16:20:42 -0500

    keeping data if any service goes down, and then restarted, working

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function CommentList({ comments }) {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") content = comment.content;
+    if (comment.status === "pending") content = "Comment is pending approval";
+    if (comment.status === "rejected") content = "Comment has been rejected";
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <div>{renderedComments}</div>;
